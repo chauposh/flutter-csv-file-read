88c106939c7aa820a15eb99c78525050a0d3a5f5
Hossein Vejdani <vejdani.shojae@gmail.com>
2020-04-08 09:17:07 +0430

    minor change . setState needed

--- lib/main.dart
+++ lib/main.dart
@@ -22,28 +22,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   //
 
-  List<List<dynamic>> data = [];
+  List<List<dynamic>> data;
 
-  loadAsset() async {
-    final myData = await rootBundle.loadString("assets/data/products.csv");
-    List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
-    data = csvTable;
+  @override
+  void initState() {
+    loadAsset();
+    super.initState();
   }
 
+  loadAsset() async {
+    var myData = await rootBundle.loadString("assets/data/category.csv");
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
+    setState(() {
+      data = csvTable;
+    });
+  }
+
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('network test'),
       ),
-      body: Center(
-        child: RaisedButton(
-            child: Text('press me'),
-            onPressed: () async {
-              await loadAsset();
-              print(data[0][1]);
-            }),
-      ),
+      body: Center(child: Text(data != null ? data[0][1] : 'data is null')),
     );
   }
 }
